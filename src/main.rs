68a1fa5e3c88b50da662b41d7742d5a872a134d4
AlexPayment <3332420+AlexPayment@users.noreply.github.com>
2024-03-07 12:20:49 -0800

    Add minimal dependency to output hello world through RTT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    rprintln!("Hello World!");
+
+    loop {}
 }
