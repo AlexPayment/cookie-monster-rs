b77fab575bbf72aa330f1e6b62628b07df50452d
AlexPayment <3332420+AlexPayment@users.noreply.github.com>
2024-03-07 14:52:20 -0800

    Print hello world in debug only

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 
 use cortex_m_rt::entry;
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::{debug_rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    rprintln!("Hello World!");
+    debug_rprintln!("Hello World!");
 
     loop {}
 }
