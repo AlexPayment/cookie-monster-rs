9c1054217514ac704d72148ba05c1afbacf1c13a
AlexPayment <3332420+AlexPayment@users.noreply.github.com>
2024-03-08 18:30:52 -0800

    Add modules to handle button A & B

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,49 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit::{hal, Peripherals};
-use microbit::hal::{spi, Timer};
+use microbit::{Board, hal};
 use microbit::hal::gpio::Level;
-use microbit::hal::gpio::p0::Parts;
+use microbit::hal::spi;
 use panic_rtt_target as _;
 use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
 use rtt_target::{debug_rprintln, rtt_init_print};
-use ws2812_spi::Ws2812;
+
+use cookie_monster::CookieMonster;
+
+use crate::controls::init_buttons;
+
+mod controls;
+mod cookie_monster;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
     // Setup all peripherals and the WS2812 device
-    let peripherals = Peripherals::take().unwrap();
-    let port0 = Parts::new(peripherals.P0);
-    let sck = port0.p0_17.into_push_pull_output(Level::Low).degrade();
+    let board = Board::take().unwrap();
+    let sck = board.pins.p0_17.into_push_pull_output(Level::Low).degrade();
     // The SPI MOSI pin is pin 15 on the micro:bit.
-    let mosi = port0.p0_13.into_push_pull_output(Level::Low).degrade();
-    let miso = port0.p0_01.into_floating_input().degrade();
+    let mosi = board.pins.p0_13.into_push_pull_output(Level::Low).degrade();
+    let miso = board.pins.p0_01.into_floating_input().degrade();
     let pins = spi::Pins {
         sck,
         miso: Some(miso),
         mosi: Some(mosi),
     };
-    let spi = spi::Spi::new(peripherals.SPI0, pins, spi::Frequency::M4, spi::MODE_0);
-    let ws2812 = Ws2812::new(spi);
+    // let spi = spi::Spi::new(peripherals.SPI0, pins, spi::Frequency::M4, spi::MODE_0);
+    // let ws2812 = Ws2812::new(spi);
 
     // Setup Pseudo Random Number Generator
-    let mut rng = hal::Rng::new(peripherals.RNG);
+    let mut rng = hal::Rng::new(board.RNG);
     let mut prng = SmallRng::seed_from_u64(rng.random_u64());
 
     let mut randos = [0x00u8; 64];
     prng.fill(&mut randos);
     debug_rprintln!("Randoms: {:?}", randos);
 
-    let timer = Timer::new(peripherals.TIMER0);
+    let cookie_monster = CookieMonster::new(board.display_pins, board.TIMER0);
+    init_buttons(board.GPIOTE, board.buttons, cookie_monster);
 
     loop {}
 }
