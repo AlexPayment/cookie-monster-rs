91c3318883d77d4d1e9a0b2fb5d83c4df96a78eb
AlexPayment <3332420+AlexPayment@users.noreply.github.com>
2024-03-19 19:23:48 -0700

    Cycle through brightness with button A

--- src/effects/mod.rs
+++ src/effects/mod.rs
@@ -1,14 +1,18 @@
 use core::cell::RefCell;
+use cortex_m::interrupt::{free, Mutex};
 use embedded_time::duration::Milliseconds;
 use embedded_time::fixed_point::FixedPoint;
 use microbit::hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 use microbit::hal::{spi, Timer};
 use rand::prelude::SmallRng;
 use rand::{Rng, SeedableRng};
+use rtt_target::rprintln;
 use smart_leds::{SmartLedsWrite, RGB8};
 use ws2812_spi::Ws2812;
 
 pub(crate) const NUM_LEDS: usize = 256;
+
+pub static SETTINGS: Mutex<RefCell<Option<Settings>>> = Mutex::new(RefCell::new(None));
 
 pub(crate) trait Effect {
     fn render(
@@ -17,6 +21,7 @@ pub(crate) trait Effect {
     );
 }
 
+#[derive(Clone, Copy)]
 pub(crate) struct Brightness {
     value: f32,
 }
@@ -45,6 +50,7 @@ impl Speed {
     pub(crate) const FASTEST: Speed = Speed { value: 10 };
 }
 
+#[derive(Clone, Copy)]
 pub(crate) struct Settings {
     brightness: usize,
     brightnesses: [Brightness; 7],
@@ -72,8 +78,10 @@ impl Settings {
 
     pub(crate) fn cycle_brightness(&mut self) {
         if self.brightness >= self.brightnesses.len() - 1 {
+            rprintln!("Resetting brightness to 0");
             self.brightness = 0;
         } else {
+            rprintln!("Increasing brightness by 1 to {}", self.brightness + 1);
             self.brightness += 1;
         }
     }
@@ -86,16 +94,11 @@ impl Settings {
 pub(crate) struct ForwardWave<'a> {
     data: &'a RefCell<[RGB8; NUM_LEDS]>,
     position: usize,
-    settings: &'a Settings,
 }
 
 impl<'a> ForwardWave<'a> {
-    pub(crate) fn new(data: &'a RefCell<[RGB8; NUM_LEDS]>, settings: &'a Settings) -> Self {
-        ForwardWave {
-            data,
-            position: 0,
-            settings,
-        }
+    pub(crate) fn new(data: &'a RefCell<[RGB8; NUM_LEDS]>) -> Self {
+        ForwardWave { data, position: 0 }
     }
 }
 
@@ -106,16 +109,27 @@ impl Effect for ForwardWave<'_> {
     ) {
         reset_data(self.data);
 
+        let mut brightness = Brightness::FIFTY.value;
+        let mut color = RGB8::new(0x00, 0x00, 0xff);
+        let mut delay_settings = Speed::MEDIUM.value;
+
+        free(|cs| {
+            if let Some(settings) = SETTINGS.borrow(cs).borrow().as_ref() {
+                brightness = settings.get_brightness().value;
+                color = settings.color;
+                delay_settings = settings.delay.integer();
+            }
+        });
+
         let wave = [
-            self.settings.get_brightness().value / 10.0,
-            self.settings.get_brightness().value / 6.0,
-            self.settings.get_brightness().value / 4.0,
-            self.settings.get_brightness().value,
-            self.settings.get_brightness().value / 10.0,
+            brightness / 10.0,
+            brightness / 6.0,
+            brightness / 4.0,
+            brightness,
+            brightness / 10.0,
         ];
         for (i, item) in wave.iter().enumerate() {
-            self.data.borrow_mut()[self.position + i] =
-                create_color_with_brightness(&self.settings.color, item);
+            self.data.borrow_mut()[self.position + i] = create_color_with_brightness(&color, item);
         }
         self.position += 1;
         if self.position >= NUM_LEDS - wave.len() {
@@ -123,24 +137,20 @@ impl Effect for ForwardWave<'_> {
         }
 
         ws2812.write(self.data.borrow().iter().cloned()).unwrap();
-        delay.delay_ms(self.settings.delay.integer() as u16);
+        delay.delay_ms(delay_settings as u16);
     }
 }
 
 pub(crate) struct UniColorSparkle<'a> {
     data: &'a RefCell<[RGB8; NUM_LEDS]>,
     prng: SmallRng,
-    settings: &'a Settings,
 }
 
 impl<'a> UniColorSparkle<'a> {
-    pub(crate) fn new(
-        data: &'a RefCell<[RGB8; NUM_LEDS]>, settings: &'a Settings, random_seed: u64,
-    ) -> Self {
+    pub(crate) fn new(data: &'a RefCell<[RGB8; NUM_LEDS]>, random_seed: u64) -> Self {
         UniColorSparkle {
             data,
             prng: SmallRng::seed_from_u64(random_seed),
-            settings,
         }
     }
 }
@@ -152,19 +162,28 @@ impl Effect for UniColorSparkle<'_> {
     ) {
         reset_data(self.data);
 
+        let mut brightness = Brightness::FIFTY.value;
+        let mut color = RGB8::new(0x00, 0x00, 0xff);
+        let mut delay_settings = Speed::MEDIUM.value;
+
+        free(|cs| {
+            if let Some(settings) = SETTINGS.borrow(cs).borrow().as_ref() {
+                brightness = settings.get_brightness().value;
+                color = settings.color;
+                delay_settings = settings.delay.integer();
+            }
+        });
+
         // The amount of sparkles, up to 10% of the total number of LEDs
         let sparkle_amount = self.prng.gen_range(0..(NUM_LEDS / 10));
         for _ in 0..sparkle_amount {
             let index = self.prng.gen_range(0..NUM_LEDS);
             // Random brightness between 1% and the set brightness
-            let brightness = self
-                .prng
-                .gen_range(Brightness::ONE.value..self.settings.get_brightness().value);
-            self.data.borrow_mut()[index] =
-                create_color_with_brightness(&self.settings.color, &brightness);
+            let brightness = self.prng.gen_range(0.0..brightness);
+            self.data.borrow_mut()[index] = create_color_with_brightness(&color, &brightness);
         }
 
-        let random_delay = self.prng.gen_range(Speed::FASTEST.value..self.settings.delay.integer());
+        let random_delay = self.prng.gen_range(Speed::FASTEST.value..delay_settings);
 
         ws2812.write(self.data.borrow().iter().cloned()).unwrap();
         delay.delay_ms(random_delay as u16);
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,24 @@
 #![no_main]
 
 use core::cell::RefCell;
+use cortex_m::interrupt::{free, Mutex};
 use cortex_m_rt::entry;
-use effects::{Effect, ForwardWave, Settings, Speed, UniColorSparkle, NUM_LEDS};
-use microbit::hal::gpio::p0::Parts;
-use microbit::hal::gpio::Level;
+use effects::{Effect, ForwardWave, Settings, Speed, UniColorSparkle, NUM_LEDS, SETTINGS};
+use microbit::hal::gpio::p0::{Parts, P0_14, P0_23};
+use microbit::hal::gpio::{Floating, Input, Level};
+use microbit::hal::gpiote::Gpiote;
 use microbit::hal::{spi, Timer};
-use microbit::{hal, Peripherals};
+use microbit::pac::{interrupt, GPIOTE};
+use microbit::{hal, pac, Peripherals};
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 use smart_leds::RGB8;
 use ws2812_spi::Ws2812;
 
-mod controls;
 mod cookie_monster;
 mod effects;
+
+static GPIO: Mutex<RefCell<Option<Gpiote>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -40,21 +44,80 @@ fn main() -> ! {
     // Setup Pseudo Random Number Generator
     let mut rng = hal::Rng::new(peripherals.RNG);
 
-    // let cookie_monster = CookieMonster::new(board.display_pins, board.TIMER0);
-    // init_buttons(board.GPIOTE, board.buttons, cookie_monster);
-
-    let data = RefCell::new([RGB8::default(); NUM_LEDS]);
-    let settings = Settings::new(
-        RGB8::new(0x00, 0x00, 0xff),
-        Speed::SLOW,
+    init_buttons(
+        peripherals.GPIOTE,
+        port0.p0_14.into_floating_input(),
+        port0.p0_23.into_floating_input(),
     );
 
-    let mut uni_color_sparkle = UniColorSparkle::new(&data, &settings, rng.random_u64());
-    let mut forward_wave = ForwardWave::new(&data, &settings);
+    let data = RefCell::new([RGB8::default(); NUM_LEDS]);
+    let settings = Settings::new(RGB8::new(0x00, 0x00, 0xff), Speed::SLOW);
+    free(|cs| {
+        SETTINGS.borrow(cs).replace(Some(settings));
+    });
+
+    let mut uni_color_sparkle = UniColorSparkle::new(&data, rng.random_u64());
+    let mut forward_wave = ForwardWave::new(&data);
 
     let effect: [&mut dyn Effect; 2] = [&mut uni_color_sparkle, &mut forward_wave];
 
     loop {
         effect[0].render(&mut ws2812, &mut delay);
     }
+}
+
+fn init_buttons(
+    board_gpiote: GPIOTE, button_a_pin: P0_14<Input<Floating>>,
+    button_b_pin: P0_23<Input<Floating>>,
+) {
+    let gpiote = Gpiote::new(board_gpiote);
+
+    let channel0 = gpiote.channel0();
+    channel0
+        .input_pin(&button_a_pin.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel0.reset_events();
+
+    let channel1 = gpiote.channel1();
+    channel1
+        .input_pin(&button_b_pin.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel1.reset_events();
+
+    free(move |cs| {
+        *GPIO.borrow(cs).borrow_mut() = Some(gpiote);
+
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::GPIOTE);
+        }
+        pac::NVIC::unpend(pac::Interrupt::GPIOTE);
+    });
+}
+
+#[interrupt]
+fn GPIOTE() {
+    free(|cs| {
+        if let Some(gpiote) = GPIO.borrow(cs).borrow().as_ref() {
+            let a_pressed = gpiote.channel0().is_event_triggered();
+            let b_pressed = gpiote.channel1().is_event_triggered();
+
+            // TODO: Implement button press handling
+            if a_pressed {
+                // Cycle brightness
+                SETTINGS
+                    .borrow(cs)
+                    .borrow_mut()
+                    .as_mut()
+                    .unwrap()
+                    .cycle_brightness();
+            } else if b_pressed {
+                // Cycle effect
+            }
+
+            gpiote.channel0().reset_events();
+            gpiote.channel1().reset_events();
+        }
+    });
 }
