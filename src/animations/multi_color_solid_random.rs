c34bddc1a6db36daa76bfe0d35c7f208983d5f38
Alexandre Payment <3332420+AlexPayment@users.noreply.github.com>
2025-03-27 20:52:37 -0700

    Update to edition 2024 (#14)

--- src/animations/multi_color_solid_random.rs
+++ src/animations/multi_color_solid_random.rs
@@ -1,9 +1,9 @@
 use crate::animations;
-use crate::animations::{Animation, MultiColorSolidRandom, Settings, NUM_LEDS};
+use crate::animations::{Animation, MultiColorSolidRandom, NUM_LEDS, Settings};
 use core::cell::RefCell;
 use embedded_hal::delay::DelayNs;
-use microbit::hal::spi::Spi;
 use microbit::hal::Timer;
+use microbit::hal::spi::Spi;
 use microbit::pac::{SPI0, TIMER0};
 use rand::prelude::SmallRng;
 use rand::{Rng, SeedableRng};
