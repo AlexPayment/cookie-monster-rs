4cb6380226c2a9104d02de091d866cc3db1c4388
AlexPayment <3332420+AlexPayment@users.noreply.github.com>
2025-05-20 14:52:01 -0700

    Handle button to switch animation

--- quinled_dig_quad/src/main.rs
+++ quinled_dig_quad/src/main.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
+use esp_hal::gpio::Pin;
 use esp_hal::timer::timg::TimerGroup;
 use {esp_backtrace as _, esp_println as _};
+
+use crate::controls::animation_button_task;
+
+static ANIMATION: usize = 0;
+const NUM_ANIMATIONS: usize = 14;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -19,11 +25,20 @@ async fn main(spawner: Spawner) {
 
     info!("Embassy initialized!");
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    // GPIO15 is the Q1 pin on the board, it's pulled low. Which means the button should also be
+    // connected to a 3.3 V pin.
+    let animation_button = peripherals.GPIO15.degrade();
+    // Spawn the animation button task
+    unwrap!(spawner.spawn(animation_button_task(
+        animation_button,
+        ANIMATION,
+        NUM_ANIMATIONS
+    )));
 
     loop {
         info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
 }
+
+mod controls;
